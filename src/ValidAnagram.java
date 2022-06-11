473da7e6337081e4d4566417d3a1c0d2df1b4664
Ray Yuan Liu <louis383@gmail.com>
2022-06-11 14:14:25 +0800

    Update ValidAnagram with second solution

--- src/ValidAnagram.java
+++ src/ValidAnagram.java
@@ -1,12 +1,6 @@
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
-import java.util.function.Consumer;
-import java.util.function.IntFunction;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class ValidAnagram {
     public static void main(String[] args) {
@@ -14,12 +8,30 @@ public class ValidAnagram {
 //        "nagaram"
 //        "rat"
 //        "car"
-//        String input = "anagram";
-//        String output = "nagaram";
-        String input = "rat";
-        String output = "car";
-        boolean isAnagram = isAnagram(input, output);
+        String input01 = "anagram";
+        String output01 = "nagaram";
+        String input02 = "rat";
+        String output02 = "car";
+
+        boolean isAnagram = isAnagram02(input02, output02);
         System.out.println("isAnagram? = " + isAnagram);
+    }
+
+    private static boolean isAnagram02(String input01, String input02) {
+        // Edge case
+        if (input01.length() != input02.length()) {
+            return false;
+        }
+
+        // Change to Java's charArray: char[]
+        // Basically char is a representation of ASCII code (it's int)
+        // Sort both with Arrays.sort
+
+        char[] chars01 = input01.toCharArray();
+        char[] chars02 = input02.toCharArray();
+        Arrays.sort(chars01);
+        Arrays.sort(chars02);
+        return Arrays.equals(chars01, chars02);
     }
 
     public static boolean isAnagram(String s, String t) {
