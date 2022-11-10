1bc08d7240286eee6177ff2320b4c81dcf1a022d
Ray Yuan Liu <louis383@gmail.com>
2022-11-10 09:02:07 +0800

    Update MergeSortedArray

--- src/MergeSortedArray.java
+++ src/MergeSortedArray.java
@@ -1,7 +1,19 @@
 import java.util.Arrays;
 
+import utils.ArrayUtils;
+
 public class MergeSortedArray {
 	public static void main(String[] args) {
+		var sampleClass = new MergeSortedArray();
+
+		// expected: [1, 2, 2, 3, 5, 6]
+		var example01A = new int[]{1, 2, 3, 0, 0, 0};
+		var example02B = new int[]{2, 5, 6};
+		var examplem1 = 3;
+		var examplem2 = 3;
+
+		sampleClass.merge02(example01A, examplem1, example02B, examplem2);
+		ArrayUtils.printArray(example01A);
 	}
 
 	// Brute force
@@ -14,4 +26,31 @@ public class MergeSortedArray {
 		}
 		Arrays.sort(nums1);
     }
+
+	// Brute force
+	public void merge02(int[] nums1, int m, int[] nums2, int n) {
+		int insertedIndex = (m + n) - 1;
+		int currentM = m - 1;
+		int currentN = n - 1;
+		while (currentM >= 0 && currentN >= 0) {
+			if (nums1[currentM] > nums2[currentN]) {
+				// M > N
+				nums1[insertedIndex] = nums1[currentM];
+				currentM--;
+			} else {
+				// M <= N
+				nums1[insertedIndex] = nums2[currentN];
+				currentN--;
+			}
+
+			insertedIndex--;
+		}
+
+		// We should make sure all numbers in nums2 has been iterated.
+		while (currentN >= 0) {
+			nums1[insertedIndex] = nums2[currentN];
+			currentN--;
+			insertedIndex--;
+		}
+	}
 }
