12865fcbc35d2b108a22ae7aa3c62ac260c51ddc
Ray Yuan Liu <louis383@gmail.com>
2023-02-17 09:53:18 +0800

    Update ReverseLinkedList

--- src/ReverseLinkedList.java
+++ src/ReverseLinkedList.java
@@ -1,5 +1,3 @@
-import java.util.List;
-
 import utils.ListNode;
 
 public class ReverseLinkedList {
@@ -19,9 +17,30 @@ public class ReverseLinkedList {
 		sample04.next = sample05;
 		//endregion
 
-		var nodes = sampleClass.reverseList(sample01);
+		var nodes = sampleClass.reverseList03(sample01);
 		ListNode.printListNode(nodes);
 
+	}
+
+	// Third practice
+	public ListNode reverseList03(ListNode head) {
+		// base case
+		if (head == null || head.next == null) {
+			// The last node should return itself, so return head.
+			return head;
+		}
+
+		ListNode reversedList = reverseList03(head.next); // Will bring the last node back
+
+		// Swapping
+		head.next.next = head; // Point the next node's node to head itself
+
+		// clean up the next node.
+		// If we didn't go through all nodes in path, this value would be fix. Otherwise,
+		// the first node of our answer should point to null.
+		head.next = null;
+		
+		return reversedList;
 	}
 
 	// Approach01: Iteratively
