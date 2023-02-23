d7eb345e407409ca18d3b644c1dfc9eb1537874f
Ray Yuan Liu <louis383@gmail.com>
2023-02-23 09:40:51 +0800

    Update MaximumDepthOfBinaryTree

--- src/MaximumDepthOfBinaryTree.java
+++ src/MaximumDepthOfBinaryTree.java
@@ -1,38 +1,59 @@
 import utils.TreeNode;
 
 public class MaximumDepthOfBinaryTree {
-    public static void main(String[] args) {
-    }
+	public static void main(String[] args) {
+	}
 
-    public int maxDepthBottomUp(TreeNode root) {
-        // bottom-up solution
-        if (root == null) {
-            return 0;
-        }
+	// Third Practice
 
-        int leftNodeCounts = maxDepthBottomUp(root.left);
-        int rightNodeCounts = maxDepthBottomUp(root.right);
-        return Math.max(leftNodeCounts, rightNodeCounts) + 1;
-    }
+	private int depth = 0;
 
-    int depthCount = 0;
-    public int maxDepthTopDown(TreeNode root) {
-        // top-down solution
-        traverse(root, 1);
-        return depthCount;
-    }
+	public int maxDepth(TreeNode root) {
+		traverse02(root, 0);
+		return depth;
+	}
 
-    private void traverse(TreeNode root, int depthCounts) {
-        if (root == null) {
-            return;
-        }
+	private void traverse02(TreeNode root, int currentDepth) {
+		if (root == null) {
+			return;
+		}
 
-        if (root.left == null && root.right == null) {
-            // Compare which counts of the maximum depth is the deepest one.
-            this.depthCount = Math.max(this.depthCount, depthCounts);
-        }
+		currentDepth = currentDepth + 1;
+		depth = Math.max(currentDepth, depth);
+		traverse02(root.left, currentDepth);
+		traverse02(root.right, currentDepth);
+	}
 
-        traverse(root.left, depthCounts + 1);
-        traverse(root.right, depthCounts + 1);
-    }
+	public int maxDepthBottomUp(TreeNode root) {
+		// bottom-up solution
+		if (root == null) {
+			return 0;
+		}
+
+		int leftNodeCounts = maxDepthBottomUp(root.left);
+		int rightNodeCounts = maxDepthBottomUp(root.right);
+		return Math.max(leftNodeCounts, rightNodeCounts) + 1;
+	}
+
+	int depthCount = 0;
+
+	public int maxDepthTopDown(TreeNode root) {
+		// top-down solution
+		traverse(root, 1);
+		return depthCount;
+	}
+
+	private void traverse(TreeNode root, int depthCounts) {
+		if (root == null) {
+			return;
+		}
+
+		if (root.left == null && root.right == null) {
+			// Compare which counts of the maximum depth is the deepest one.
+			this.depthCount = Math.max(this.depthCount, depthCounts);
+		}
+
+		traverse(root.left, depthCounts + 1);
+		traverse(root.right, depthCounts + 1);
+	}
 }
