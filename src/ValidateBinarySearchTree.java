41b542d98f952dbbbd2a7664f3cc8ed3448c08e1
Ray Yuan Liu <louis383@gmail.com>
2023-04-13 09:00:26 +0800

    Update ValidateBinarySearchTree and add a TreeNode generator function

--- src/ValidateBinarySearchTree.java
+++ src/ValidateBinarySearchTree.java
@@ -2,17 +2,79 @@ import utils.TreeNode;
 
 public class ValidateBinarySearchTree {
 	public static void main(String[] args) {
+		// Expected: false
 		TreeNode node01 = new TreeNode(2);
 		TreeNode node02 = new TreeNode(2);
 		TreeNode node03 = new TreeNode(2);
 		node01.left = node02;
 		node01.right = node03;
 
+		// Expected: false
+		TreeNode node002 = TreeNode.arrayToBinaryTree(new Integer[]{5, 1, 4, null, null, 3, 6});
 		var sampleClass = new ValidateBinarySearchTree();
-		boolean result = sampleClass.isValidBST(node01);
+		boolean result = sampleClass.isValidBST03(node01);
 		System.out.println("Result is " + result);
 	}
 
+	public boolean isValidBST03(TreeNode root) {
+		if (root == null) {
+			return true;
+		}
+		// null means infinity value. (left bound is negative infinity, and right bound is infinity)
+		// we use null here to prevent input value be Math_MAX or Math_MIN
+		return traverse(root, null, null);
+	}
+
+	private boolean traverse(TreeNode node, Integer leftBound, Integer rightBound) {
+		if (node == null) {
+			return true;
+		}
+
+		if ((leftBound != null && node.val <= leftBound) ||
+			(rightBound != null && node.val >= rightBound)) {
+			return false;
+		}
+
+		// Find left and right subtree
+		return traverse(node.left, leftBound, node.val) &&
+				traverse(node.right, node.val, rightBound);
+	}
+
+
+	//region Second Try
+	private boolean isValidBst02 = true;
+	private Integer previousNumber = null;
+	public boolean isValidBST02(TreeNode root) {
+		if (root == null) {
+			return true;
+		}
+
+		traverseTree02(root);
+		return isValidBst02;
+	}
+	private void traverseTree02(TreeNode node) {
+		if (node == null) {
+			return;
+		}
+
+		// 中序遍壢(l - h - r), 如果是 BST, 在中序遍壢之中，數字會由小排列到大
+		traverseTree02(node.left);
+
+		int current = node.val;
+		if (previousNumber == null) {
+			previousNumber = current;
+		} else {
+			if (current > previousNumber) {
+				previousNumber = current;
+			} else {
+				isValidBst02 = false;
+			}
+		}
+		traverseTree02(node.right);
+	}
+	//endregion
+
+	//region First Try
 	private boolean isValidBst = true;
 	private Integer savedInteger = null;
 
@@ -41,4 +103,5 @@ public class ValidateBinarySearchTree {
 		}
 		traverseTree(root.right);
 	}
+	//endregion
 }
--- src/utils/TreeNode.java
+++ src/utils/TreeNode.java
@@ -1,5 +1,9 @@
 package utils;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import org.jetbrains.annotations.Nullable;
+
 public class TreeNode {
 	public int val;
 	public TreeNode left;
@@ -16,5 +20,53 @@ public class TreeNode {
 		this.val = val;
 		this.left = left;
 		this.right = right;
+	}
+
+	public static void main(String[] args) {
+		Integer[] inputArray = new Integer[]{1, 2, 4, 6, null, 2};
+		TreeNode root = arrayToBinaryTree(inputArray);
+		printTree(root);
+	}
+
+	@Nullable
+	public static TreeNode arrayToBinaryTree(Integer[] inputArray) {
+		if (inputArray == null || inputArray.length == 0) {
+			return null;
+		}
+
+		// generate root node
+		TreeNode treeNode = new TreeNode(inputArray[0]);
+		Queue<TreeNode> queue = new LinkedList<TreeNode>();
+		queue.offer(treeNode);
+
+		int index = 1;
+		while (!queue.isEmpty() && index < inputArray.length) {
+			TreeNode currentNode = queue.poll();
+
+			// 處理左子樹
+			if (inputArray[index] != null) {
+				currentNode.left = new TreeNode(inputArray[index]);
+				queue.offer(currentNode.left);
+			}
+			index++;
+
+			// 處理右子樹
+			if (index < inputArray.length && inputArray[index] != null) {
+				currentNode.right = new TreeNode(inputArray[index]);
+				queue.offer(currentNode.right);
+			}
+			index++;
+		}
+		return treeNode;
+	}
+
+	public static void printTree(TreeNode node) {
+		if (node == null) {
+			return;
+		}
+
+		System.out.println(node.val + " ");
+		printTree(node.left);
+		printTree(node.right);
 	}
 }
