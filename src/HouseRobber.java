91c4b707fc2bbcd3bb7bd8d2189bd36ff3957022
Ray Yuan Liu <louis383@gmail.com>
2023-08-15 22:44:33 +0800

    Update HouseRobber with iterate implementation

--- src/HouseRobber.java
+++ src/HouseRobber.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -26,5 +27,27 @@ public class HouseRobber {
 		} else {
 			return memoization.get(index);
 		}
+	}
+
+	private int robHouse02(int[] nums) {
+		if (nums == null || nums.length == 0) {
+			return 0;
+		}
+
+		int[] sums = new int[nums.length];
+		Arrays.fill(sums, 0);
+
+		for (int i = 0; i < nums.length; i++) {
+			if (i == 0) {
+				sums[i] = nums[i];
+			} else if (i == 1) {
+				var secondNum = Math.max(nums[i], sums[i - 1]);
+				sums[i] = secondNum;
+			} else {
+				var current = Math.max(nums[i] + sums[i - 2], sums[i - 1]);
+				sums[i] = current;
+			}
+		}
+		return sums[sums.length - 1];
 	}
 }
